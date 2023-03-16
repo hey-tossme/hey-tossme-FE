28a85298fa4f544789d94256ebd2d38c8f851ba7
rangggu <aliyah521@naver.com>
2023-03-16 18:48:28 +0900

    Feat : 카테고리 페이지 카드, 페이지네이션 컴포넌트 수정

--- src/components/category/CardItem.tsx
+++ src/components/category/CardItem.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default function CardItem() {
     const [bookmark, setBookmark] = useState<boolean>();
 
     const handleSetBookmark = () => {
-        setBookmark(true);
+        bookmark ? setBookmark(false) : setBookmark(true);
     };
 
     return (
@@ -20,7 +21,9 @@ export default function CardItem() {
             </div>
             <div className="item-info-area">
                 <div className="item-info-header">
-                    <p className="item-info-title">제주도 풀빌라</p>
+                    <Link to="/detail/id">
+                        <p className="item-info-title">제주도 풀빌라</p>
+                    </Link>
                     <div onClick={handleSetBookmark} className="bookmark-btn-wrapper">
                         {bookmark ? (
                             <BsBookmarkFill className="bookmark-btn" />
