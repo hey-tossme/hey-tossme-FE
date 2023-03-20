086137c4f46b41ba6e089d86271290b8f3f00a9e
rangggu <aliyah521@naver.com>
2023-03-20 21:54:03 +0900

    Refactor : 상품 카드 컴포넌트 코드 리팩토링

--- src/components/category/CardList.tsx
+++ src/components/category/CardList.tsx
@@ -16,17 +16,7 @@ export default function CardList() {
             <div className="card-list">
                 {cardList &&
                     cardList.map((item) => {
-                        return (
-                            <CardItem
-                                key={item.id}
-                                id={item.id}
-                                img={item.image_url}
-                                title={item.title}
-                                price={item.price}
-                                endDate={item.dueTime}
-                                address=""
-                            />
-                        );
+                        return <CardItem key={item.id} list={item} />;
                     })}
             </div>
         </div>
