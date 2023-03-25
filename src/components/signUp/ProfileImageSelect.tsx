9abaff6866b9f96f6fb268580d5c50e6f8a43245
trustmitt <mainmirror14@gmail.com>
2023-03-26 01:18:00 +0900

    Feat : 프로필 이미지 업로드 시 이미지 최적화

--- src/components/signUp/ProfileImageSelect.tsx
+++ src/components/signUp/ProfileImageSelect.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import profile from "../../assets/images/profile-user.png";
+import imageCompression from "browser-image-compression";
 import { HiOutlinePlus } from "react-icons/hi";
 import { IProfileFiles } from "./_SignUp.interface";
 
@@ -9,18 +10,22 @@ export default function ProfileImageSelect({
     imageSrc,
     setImageSrc,
 }: IProfileFiles) {
-    const onUpload = (e: any) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        setFiles(e.target.files[0]);
-
-        return new Promise<void>((resolve) => {
-            reader.onload = () => {
-                setImageSrc(reader.result);
-                resolve();
-            };
-        });
+    const handleFileOnChange = async (e: any) => {
+        let file = e.target.files[0];
+        const options = {
+            maxSizeMB: 2,
+            maxWidthOrHeight: 300,
+        };
+        try {
+            const compressedFile = await imageCompression(file, options);
+            setFiles(compressedFile);
+            const promise = imageCompression.getDataUrlFromFile(compressedFile);
+            promise.then((result) => {
+                setImageSrc(result);
+            });
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -43,7 +48,7 @@ export default function ProfileImageSelect({
                 name="choose-file"
                 accept="image/*"
                 className="hidden"
-                onChange={(e) => onUpload(e)}
+                onChange={(e) => handleFileOnChange(e)}
             />
         </form>
     );
