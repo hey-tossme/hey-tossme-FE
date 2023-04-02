ac4194043c8f684e8c0fa27a4980ea5518c32e32
rangggu <aliyah521@naver.com>
2023-04-03 03:19:08 +0900

    Feat : 장소 선택 유무에 따른 조건부 렌더링 기능 추가

--- src/components/detail/DetailMap.tsx
+++ src/components/detail/DetailMap.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import Map from "./Map";
 import { detailMapProps } from "./_detail.interface";
+import { useState, useEffect } from "react";
 
 export default function DetailMap({ item }: detailMapProps) {
+    const [latitude, setLatitude] = useState(item.latitude);
+    const [longitude, setLongitude] = useState(item.longitude);
+    const [isAddress, setIsAddress] = useState<boolean>(false);
+    const [isAddressDetail, setIsAddressDetail] = useState<boolean>(false);
+
+    useEffect(() => {
+        item.addressDetail && setIsAddressDetail(true);
+        item.address && setIsAddress(true);
+    }, [item]);
+
+    useEffect(() => {
+        if (!latitude && !longitude) {
+            setLatitude(37.56639);
+            setLongitude(126.978152);
+        }
+    }, [latitude, longitude]);
+
     return (
         <div className="detail-map-wrapper">
             <p className="map-title">위치</p>
             <Map latitude={item.latitude} longitude={item.longitude} />
-            <p className="address">{`${item.address}, ${item.addressDetail}`}</p>
+            <p className="address">
+                {isAddress && `${item.address}`}
+                {isAddressDetail && `, ${item.addressDetail}`}
+            </p>
         </div>
     );
 }
